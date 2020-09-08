3e65a9688a5b153c0a015f032a4a7240f1f956fa
angelhpascual <angelhpascual@gmail.com>
2020-09-08 23:08:48 +0200

    First try Material-ui added

---
+++ src/componnets/Sidebar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWVjtiJKN2aZ0VQfdPavWd_FhIUTR_rm8",
+  authDomain: "whatsapp-clone-aca5d.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-aca5d.firebaseio.com",
+  projectId: "whatsapp-clone-aca5d",
+  storageBucket: "whatsapp-clone-aca5d.appspot.com",
+  messagingSenderId: "127983503916",
+  appId: "1:127983503916:web:3d5126cdeaba4cd5f0df89",
+  measurementId: "G-ZT58FDZ534",
+};
