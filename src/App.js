f5e915525d574bd77c9568faae072db1c54b0d13
angelhpascual <angelhpascual@gmail.com>
2020-09-15 14:07:28 +0200

    react router dom imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./componnets/Sidebar";
 import Chat from "./componnets/Chat";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   //BEM naming convention
